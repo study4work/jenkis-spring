f9dad06bf05e003695f70e7f349ae78874ae6c39
FaJlJleN@yandex.ru <FaJlJleN@ya.ru>
2022-11-01 23:17:02 +0300

    commit 9

--- src/main/java/com/lysenko/springwardeploy/SpringWarDeployApplication.java
+++ src/main/java/com/lysenko/springwardeploy/SpringWarDeployApplication.java
@@ -2,8 +2,6 @@ package com.lysenko.springwardeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringWarDeployApplication {
