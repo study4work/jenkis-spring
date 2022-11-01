83fd7b8df46ac4345fef64331387ca06c796b57e
FaJlJleN@yandex.ru <FaJlJleN@ya.ru>
2022-11-02 00:54:14 +0300

    commit 14

--- src/main/java/com/lysenko/springwardeploy/DemoController.java
+++ src/main/java/com/lysenko/springwardeploy/DemoController.java
@@ -1,19 +1,13 @@
 package com.lysenko.springwardeploy;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
 
-    Logger logger = LogManager.getLogger();
-
     @GetMapping("/demo")
     public String hello() {
-        logger.log(Level.INFO, "TEST");
         return "hello";
     }
 
--- src/main/java/com/lysenko/springwardeploy/SpringWarDeployApplication.java
+++ src/main/java/com/lysenko/springwardeploy/SpringWarDeployApplication.java
@@ -2,9 +2,16 @@ package com.lysenko.springwardeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringWarDeployApplication {
+public class SpringWarDeployApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringWarDeployApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWarDeployApplication.class, args);
